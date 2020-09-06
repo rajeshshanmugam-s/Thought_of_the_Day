c34bb16a7cec8c5fc805c0489cf3fd9f7c4c2761
rajeshshanmugam <rajeshshanmugam02@gmail.com>
2020-09-06 13:19:50 +0530

    [Minor]Add quotes in email

--- mailer.py
+++ mailer.py
@@ -1,4 +1,18 @@
 import smtplib, ssl
+import json
+import random
+
+with open('data/quotes_1.json') as file:
+    data = json.load(file)
+
+def quote_finder():
+    while True:
+        rand_no = random.randint(0,1643)
+        if not data[rand_no]['Shared']:
+            data[rand_no]['Shared'] = True
+            with open('data/quotes_1.json', 'w+') as file:
+                json.dump(data, file)
+            return data[rand_no]
 
 port = 587  # For starttls
 smtp_server = "smtp.gmail.com"
@@ -7,8 +21,10 @@ receiver_email = "receiver@mail.com"
 password = "password"
 message = """\
 Subject: Hi there
+"""+quote['text']+"""\
 
-This message is sent from Python."""
+
+~"""+quote['author']
 
 context = ssl.create_default_context()
 with smtplib.SMTP(smtp_server, port) as server:
